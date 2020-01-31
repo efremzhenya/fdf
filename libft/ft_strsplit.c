6bc09fd5e711364d284b17e9d87832e70ffb95b8
Louise Seema <lseema@at-h1.21-school.ru>
2020-01-31 19:37:16 +0300

    FIX ft_strsplit!!! now work reading file

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: lseema <lseema@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 19:13:41 by lseema            #+#    #+#             */
-/*   Updated: 2019/05/27 21:39:43 by lseema           ###   ########.fr       */
+/*   Updated: 2020/01/31 19:36:36 by lseema           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,21 @@
 
 static int	ft_num_words(char const *str, char c)
 {
-	size_t	count;
-	size_t	i;
+	int i;
+	int words;
 
-	count = 0;
+	words = 0;
 	i = 0;
-	if (str[i] && (str[i] != c) && (str[i + 1] != c) && (str[i + 1]))
-		count++;
 	while (str[i])
 	{
-		if ((str[i] == c) && (str[i + 1] != c) && (str[i + 1]))
-			count++;
-		i++;
+		while (str[i] == c && str[i] != '\0')
+			i++;
+		if (str[i])
+			words++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
 	}
-	return (count);
+	return (words);
 }
 
 static int	ft_splitstr(char **arr, const char *s, char d, size_t len)
@@ -64,7 +65,7 @@ char		**ft_strsplit(char const *str, char c)
 	if (!str)
 		return (NULL);
 	l = ft_num_words(str, c);
-	if (!(arr = ((char **)ft_memalloc(sizeof(char *) * l + 1))))
+	if (!(arr = ((char **)ft_memalloc(sizeof(char *) * (l + 1)))))
 		return (NULL);
 	if (ft_splitstr(arr, str, c, l))
 		return (arr);
